cc6d237ccced09c25a537e21b6d9d0928abe9f4b
Aniruddha Chandra <aniruddha.282003@gmail.com>
2024-06-14 09:33:18 +0530

    Add add( ) and encode and decode property

--- Password.py
+++ Password.py
@@ -1,25 +1,45 @@
-
+import maskpass  # importing maskpass library
+import base64
 
 def view(yourId):
+    print("\n")
     file = open("password.txt","r")
     users=file.readlines()
     for user in users:
-        (userId,password) = user.split(":")
+        (name,userId,password) = user.split(" : ")
         if(yourId==userId.lower()):
-            print(f"View Mode: {userId} : {password}")
+            # Decoding the string
+            decodePassword = base64.b64decode(password[1:]).decode("utf-8")
+             
+            print(f"View Mode:\n Name: {name}\n userName: {userId}\n Password: {decodePassword}")
+            print("\n")
             return True
     return False
 
     
 def add():
-    file = open("password.txt","a")
-    file.write("\nThis will add this line77")
+    userName = input("Enter your username: ")
+    name = input("Enter your name: ")
+    print("Enter your password (mininum length of your password is 4): ")
+    password = maskpass.askpass(prompt="Password:", mask="#")
+    
+    if(len(password)<4):
+        print("Error: password is not secure, Password must heve mininum length of 7")
+        add()
+    else:
+        # Encoding the string
+        encodePassword = base64.b64encode(password.encode("utf-8"))
+        
+        file = open("password.txt","a")
+        file.write(f"{name} : {userName} : {encodePassword}\n")
 
+def edit():
+    pass
 
 def main():
     print("This is a Password Manager System")
     while(True):
-        mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit?").lower()
+        mode = input("Would you like to add a new password or view existing ones (view, add, edit), press q to quit? ").lower()
         if(mode=="q"): 
             break
         elif(mode=="view"):
@@ -28,6 +48,10 @@ def main():
                print(f"Error: This {userId} user name is not present")
         elif(mode=="add"):
             add()
+            print("Add Successfully!!")
+        elif(mode="edit"):
+            edit()
+            print("Edit Successfully!!")
         else:
             print("Invalid mode.")
     
--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-file = open("password,txt","r")
-data=file.readlines()
-print(data)
+# file = open("password,txt","r")
+# data=file.readlines()
+# print(data)
+
+import maskpass  # importing maskpass library
+import base64
+
+# prompt msg = Password and
+# masking password with hashtag(#)
+pwd = maskpass.askpass(prompt="Password:", mask="#")
+p="Aniruddha77777"
+encodePassword = base64.b64encode(p.encode("utf-8"))
+decodePassword = base64.b64decode('QW5pcnVkZGhh').decode("utf-8")
+print(p)
+print(encodePassword)
+print(decodePassword)
